5a5b18b346140ba89733790aa7050c408c205c40
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-03-28 00:26:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ $title[Help Menu]
 $description[stats - shows bot statistics
 changes/CL - shows things modified in the bot
 work - work for cash
+mine - mine for gems
 daily - gives you daily money
 weekly - weekly cash
 hack - hack for cash
@@ -127,7 +128,7 @@ $image[https://i.ibb.co/rFwpMCp/ranbom.gif]`
 bot.Command({
 	name: `work`,
 	code: `$setVar[Cash;$sum[$getVar[Cash;$authorID];$random[50;500]];$authorID]
-$title[WORK]
+$title[WORK UwU]
 $globalCooldown[10m;You can work again in {time}]
 $description[You worked and got $$random[50;500]]
 $color[RANDOM]
@@ -250,6 +251,23 @@ $footer[Executed in $ping ms | executed by $username[$authorID]]
 $addTimestamp
 $addCmdReactions[✅]
 $image[https://i.ibb.co/rFwpMCp/ranbom.gif]`
+})
+
+bdfd.command({
+name:"hacks",
+code:`**__Starting a dangerous Hack on $username[$mentioned[1]]__**
+$editIn[3s;**Got the token of the user:**
+$randomString[18];**Email of the user:**
+$replaceText[$username; ;_;-1]@gmail.com;Password:
+$randomString[7];**Recent contacts:**
+$username[$randomUserID];**Most used word:**
+$randomText[meme;lol;nah;lmao;dude;bruh;wut;nou];**Most recent dm message:**
+$randomText[I think you are mad af;bye;Why u blocked me;I am mad];**Hacking medical records**;**Hacked Email(bypassed 2FA too)**;**Injecting Latest version of Corona into the account**;**Hacking microsoft account**;**Microsoft password: $randomText[ZapIsZap@Zap.com;IAmNoob@gmail.com;ByeBye@ok.co.in;$replaceText[$username; ;_;-1]IsSmart@yahoo.com]**;**Checking User Games**;**Plays $randomText[fortnite(....);subway surfers;temple run;clash of clans]**;**Hacking epic games account**;**Epic games account hacked and deleted**;**Discord IP:
+$numberSeparator[$random[100000000;900000000];.]**;__The *totally* dangerous, risky and scary hack on $username[$mentioned[1]] is complete!__
+
+But be safe, the police may come anytime soon now :rofl:]
+$onlyIf[$mentioned[1]!=;Woah wait, where is the person who is to be hacked bruh]
+$onlyIf[$authorID!=$mentioned[1];....hacking yourself?]`
 })
 
 bdfd.command({
